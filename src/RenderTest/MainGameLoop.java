d4c73436a9ee2322db436e4cf3decf9530011893
phipps980316 <59151421+phipps980316@users.noreply.github.com>
2020-09-02 23:10:16 +0100

    Added Classes To The RenderEngine Package

--- src/RenderTest/MainGameLoop.java
+++ src/RenderTest/MainGameLoop.java
@@ -1,14 +1,35 @@
 package RenderTest;
 
 import RenderEngine.DisplayManager;
+import RenderEngine.ModelLoader;
+import RenderEngine.ModelRenderer;
+import RenderEngine.RawModel;
 import org.lwjgl.opengl.Display;
 
 public class MainGameLoop {
     public static void main(String[] args) {
         DisplayManager.createDisplay();
+        ModelLoader modelLoader = new ModelLoader();
+        ModelRenderer modelRenderer = new ModelRenderer();
+
+        float[] vertices = {
+                -0.5f, 0.5f, 0f,
+                -0.5f, -0.5f, 0f,
+                0.5f, -0.5f, 0f,
+                0.5f, -0.5f, 0f,
+                0.5f, 0.5f, 0f,
+                -0.5f, 0.5f, 0f
+        };
+
+        RawModel model = modelLoader.loadToVAO(vertices);
+
         while(!Display.isCloseRequested()){
+            modelRenderer.prepare();
+            modelRenderer.render(model);
             DisplayManager.updateDisplay();
         }
+
+        modelLoader.cleanUp();
         DisplayManager.closeDisplay();
     }
 }
